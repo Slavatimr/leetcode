9b517d720d7d48195b085ad4aed57730354ff92c
slavatimr <tsimiertsiev@gmail.com>
2024-12-23 18:45:22 +0300

    solved

--- solutions/valid-palindrome.py
+++ solutions/valid-palindrome.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join([char for char in s.lower() if char.isalpha()])
+        s = "".join([char for char in s.lower() if char.isalnum()])
         return s == s[::-1]
